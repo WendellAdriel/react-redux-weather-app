7174313e969ecce3314c6b0f06a137063f9bfc16
Wendell Adriel <wendelladriel.ti@gmail.com>
2017-05-02 13:42:43 +0100

    initial weather app structure

---
+++ README.md
@@ -0,0 +1,3 @@
+# React and Redux Weather App
+
+> Simple weather app using React and Redux
---
+++ src/components/app.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+import SearchBar from '../containers/search-bar'
+
+export default class App extends Component {
+  render() {
+    return (
+      <div>
+        <SearchBar />
+      </div>
+    )
+  }
+}
