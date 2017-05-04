e8cabb5757e1a4f3edc344dbdfd38b2ef189338d
Wendell Adriel <wendelladriel.ti@gmail.com>
2017-05-04 19:07:14 +0100

    removed don't used constructor

--- src/components/google-map.js
+++ src/components/google-map.js
@@ -1,10 +1,6 @@
 import React, { Component } from 'react'
 
 export default class GoogleMap extends Component {
-  constructor (props) {
-    super(props)
-  }
-
   componentDidMount () {
     new google.maps.Map(this.refs.map, {
       zoom: 12,
